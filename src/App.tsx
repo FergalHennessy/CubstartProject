25609b6f01bba747dd9c0ef20ee1f3e1964f9c24
Fergal Hennessy <fergalhennessy@berkeley.edu>
2022-04-26 22:01:34 -0700

    changing initial

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 
 function App() {
   return (
-      <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={true}>
+      <MapContainer center={[37.869061, -122.270462]} zoom={13} scrollWheelZoom={true}>
   <TileLayer
     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
