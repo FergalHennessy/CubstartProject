e394c78f8b511d37e32e1447e796c51ecb9ed63f
Fergal Hennessy <fergalhennessy@berkeley.edu>
2022-04-26 23:27:44 -0700

    move mark

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
   />
-  <Marker position={[51.505, -0.09]}>
+  <Marker position={[37.869061, -122.270462]}>
     <Popup>
       A pretty CSS3 popup. <br /> Easily customizable.
     </Popup>
