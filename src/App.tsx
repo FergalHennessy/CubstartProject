a93cc8750c17a3aa65892b381856626816456e76
Fergal Hennessy <fergalhennessy@berkeley.edu>
2022-04-28 19:07:33 -0700

    dynamic pictures

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import { MapContainer, TileLayer, Marker, Popup} from 'react-leaflet'
 import L from 'leaflet'
 import { createLogicalOr } from 'typescript';
 import { logDOM } from '@testing-library/react';
@@ -15,25 +15,42 @@ function GetIcon(_iconSize: any, _whichIcon: string){
 
 function App() {
 
-  //DEFINE SOME CONSTANT IN HERE WITH DATE PULLED FROM CENTRAL SERVER
+
+  const locations  = [
+    {"position": [37.869061, -122.270462], "size": 200, icon:"test"},
+    {"position": [37.769061, -122.470462], "size": 200, icon:"test1"} 
+  ]
+  
 
   return (
-      <MapContainer center={[37.869061, -122.270462]} zoom={13} scrollWheelZoom={true}>
-  <TileLayer
-    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-  />
-  <Marker position={[37.869061, -122.270462]} icon={GetIcon(200, "test")}>
-    <Popup>
-      A pretty CSS3 popup. <br /> Easily customizable.
-    </Popup>
-  </Marker>
-  <Marker position={[37.969061, -122.070462]} icon={GetIcon(200, "test1")}>
-    <Popup>
-      A pretty CSS3 popup. <br /> Easily customizable.
-    </Popup>
-  </Marker>
+    <>
+    <MapContainer center={[37.869061, -122.270462]} zoom={13} scrollWheelZoom={true}>
+        
+    
+
+
+
+    {locations.map((location)=>(
+      <Marker position={location.position as [number, number]} icon={GetIcon(location.size, location.icon)}>
+          <Popup>
+            <div>testing</div>
+         </Popup>
+     </Marker>
+    ))}
+        
+    
+
+
+
+
 </MapContainer>
+
+
+    <label className="custom-file-upload" z-index={1000}>
+            <input type="file" id="img-upload" accept="image/*" title=" "/>
+            Upload Image
+        </label>
+      </>
   );
 }
 
